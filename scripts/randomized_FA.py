54bb9ba2234e1969d66f4838eb50173b15fa008b
stereopickle <stereopickle@gmail.com>
2021-01-15 11:28:51 -0500

    added find_files function

--- scripts/randomized_FA.py
+++ scripts/randomized_FA.py
@@ -10,14 +10,36 @@ new numbers and generate a key reference of old-new name match
 
 """
 
+import re
+import os
 
-def find_files(dir, fpath):
-    ''' find the name of file in the directory ''' 
-    pass
+def find_files(list_of_dir, dir_word = 'sub', file_word = 'FA'):
+    ''' 
+    from the list of directories
+    find sub-directories starting with the dir_word
+    within these sub-directories, 
+    get a list of files (full path) conatining the file_word
+    ''' 
+    paths = []
+    for dir_ in list_of_dir: 
+        path_ = [x[0] for x in os.walk(dir_) if x[0].startswith(dir_word)]
+        full_path = os.join(dir_, path_)
+        paths.append(full_path)
+        
+    full_paths = []
+    for fp in paths:
+        fnames = [x for x in os.listdir(fp) if file_word in x]
+        full_path = os.join(fp, fnames)
+        full_paths.join(full_path)
+        
+    return full_paths
+        
 
 def copy_files(fpath, new_path): 
     ''' copy over a file to the new path '''
     pass
+ 
+
 
 def random_assignment(fpath):
     ''' 
