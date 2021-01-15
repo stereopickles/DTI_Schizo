5aacfef944c629cf1211fb62b9e83c835948575c
stereopickle <stereopickle@gmail.com>
2021-01-15 11:03:54 -0500

    updating the overall structure

--- scripts/randomized_FA.py
+++ scripts/randomized_FA.py
@@ -1,32 +1,43 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 10:51:16 2021
+
+@author: Eunjoo Byeon
+
+A script to copy over existing files and rename them to 
+new numbers and generate a key reference of old-new name match
+
+"""
+
+
+def find_files(dir, fpath):
+    ''' find the name of file in the directory ''' 
+    pass
+
+def copy_files(fpath, new_path): 
+    ''' copy over a file to the new path '''
+    pass
+
+def random_assignment(fpath):
+    ''' 
+    extract the current subject number, 
+    assign a new number, 
+    return a dictionary of old/new
+    '''
+    pass
+
+def rename_files(fpath, old_name, new_name):
+    pass
+
+
+def rand_assignment_processing():
+    '''
+    take a directory then rename all files 
+    and return the dictionary of old-new file name references
+    '''
+    ref = random_assignment(fpath)
+    for old, new in ref: 
+        rename_files(fpath, old, new)
+    
+    return ref
